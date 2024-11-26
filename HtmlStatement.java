afcd6c8d9deaebbbe48bc4f73f571ec3b7521839
jorgebbastos <jorgebervianbastos@gmail.com>
2024-11-25 21:28:05 -0300

    commit 16

---
+++ HtmlStatement.java
@@ -0,0 +1,5 @@
+class Customer {
+    public String htmlStatement() {
+        return new HtmlStatement().value(this);
+    }
+}
---
+++ Statement.java
@@ -0,0 +1,6 @@
+import java.util.Enumeration;
+
+public abstract class Statement {
+    
+    public abstract String value(Customer aCustomer);
+}
